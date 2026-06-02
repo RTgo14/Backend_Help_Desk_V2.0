4f7b6dfaaf8061dee8ed382e1b5e3540e481193d
MichaelAP19 <palmamichael464@gmail.com>
2026-06-01 19:17:11 -0500

    Actualizacion routes

--- src/routes/incidencia.routes.js
+++ src/routes/incidencia.routes.js
@@ -1,9 +1,19 @@
-import { Router } from 'express';
-import { getIncidencias } from '../controllers/incidencia.controller.js';
+import { Router } from "express";
+import {
+    getIncidencias,
+    getIncidenciaById,
+    createIncidencia,
+    updateIncidencia,
+    deleteIncidencia,
+} from "../controllers/incidencia.controller.js";
+import { validarIncidencia } from "../middlewares/validator.middleware.js";
 
 const router = Router();
 
-//Endpoint GET /api/incidencias
-router.get('/', getIncidencias);
+router.get("/", getIncidencias);
+router.get("/:id", getIncidenciaById);
+router.post("/", validarIncidencia, createIncidencia);
+router.put("/:id", validarIncidencia, updateIncidencia);
+router.delete("/:id", deleteIncidencia);
 
 export default router;
